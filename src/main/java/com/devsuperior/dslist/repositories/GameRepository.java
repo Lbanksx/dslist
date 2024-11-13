025acc47c10fa0cb8e814d5fb4f3095dee2c83dc
Hugo <hugonsantos03@gmail.com>
2024-11-12 22:47:35 -0300

    Consulta SQL, projection, busca de games por lista

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -1,9 +1,23 @@
 package com.devsuperior.dslist.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public interface GameRepository extends JpaRepository<Game, Long>{
 
+	@Query(nativeQuery = true, value = """
+			SELECT game.id, game.title, game.game_year AS `year`, game.img_url AS imgUrl,
+			game.short_description AS shortDescription, belonging.position
+			FROM game
+			INNER JOIN belonging ON game.id = belonging.game_id
+			WHERE belonging.game_list_id = :listId
+			ORDER BY belonging.position
+		""")
+	List<GameMinProjection> searchByList(Long listId);
 }
+
